11ec934c7adc07c346a605cc0d5c4c7105da6838
Alfonso Palacio <alfonsopalacio@live.com>
2019-01-29 17:56:57 -0500

    Adding component

--- albums/index.js
+++ albums/index.js
@@ -3,8 +3,19 @@
  * @lint-ignore-every XPLATJSCOPYRIGHT1
  */
 
-import {AppRegistry} from 'react-native';
-import App from './App';
-import {name as appName} from './app.json';
+// Import a Library to help create a component
+import React from 'react';
+import ReactNative from 'react-native';
 
-AppRegistry.registerComponent(appName, () => App);
+
+
+// Create a component
+const App = () => {
+    return (
+        <Text>Some Text</Text>
+    );
+};
+
+
+// Render it to the device
+
