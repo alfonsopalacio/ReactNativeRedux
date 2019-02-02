ae78340f8524b1584978324eb9bd08da18da5b6b
Alfonso Palacio <alfonsopalacio@live.com>
2019-02-02 12:53:44 -0500

    Adding styling to Header component and new Component AlbumList.

--- albums/index.js
+++ albums/index.js
@@ -6,12 +6,12 @@
 // Import a library to help create a component
 import React from 'react';
 import { Text , AppRegistry } from 'react-native';
-import Header from './src/components/header';
+import Header from './src/components/Header';
 
 
 // Create a component
 const App = () => (
-  <Header />
+  <Header headerText={'Albums'} />  //pasando el prop headerText
 );
 
 // Render it to the device
--- albums/src/components/header.js
+++ albums/src/components/header.js
@@ -6,13 +6,13 @@ import { Text, View } from 'react-native';
 
 
 // Make a component
-const Header = () => {
+const Header = (props) => {
 
   const { textStyle, viewStyle } = styles;  // usando Destructuring
 
   return (
-            <View style = {viewStyle}>
-                <Text style={textStyle}>Albums</Text>
+            <View style={viewStyle}>
+                <Text style={textStyle}>{props.headerText}</Text>
             </View>
         );
   };
@@ -23,11 +23,17 @@ const Header = () => {
 
 const styles = {
   viewStyle:{
-    backgroundColor: '#F8#F8#F8',
+    backgroundColor: '#F8F8F8',
     justifyContent: 'center',
     alignItems: 'center',
     height: 60,
-    paddingTop: 15
+    paddingTop: 15,
+    //shadow stiling
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 40 },  //ancho , altura
+    shadowOpacity: 0.9,   //oscuridad de la sombra
+    elevation: 2,
+    position: 'relative'
   },
   textStyle:{
     fontSize: 20
